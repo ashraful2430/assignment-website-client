8ac0bf6e279f44aacb6e7b292587c67f8afeaa9b
ashraful2430 <www.savage582@gmail.com>
2023-11-08 21:53:05 +0600

    fixed the deploy problem

--- src/Config/firebase.config.js
+++ src/Config/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_APIKEY,
-    authDomain: import.meta.env.VITE_AUTHDOMAIN,
-    projectId: import.meta.env.VITE_PROJECTID,
-    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-    appId: import.meta.env.VITE_APPID,
+    apiKey: "AIzaSyDqwgeT4WV6NwAliIKDLyXXoK4dZpcm2Z8",
+    authDomain: "cars-doctor-958c0.firebaseapp.com",
+    projectId: "cars-doctor-958c0",
+    storageBucket: "cars-doctor-958c0.appspot.com",
+    messagingSenderId: "815479979549",
+    appId: "1:815479979549:web:912772ce582e81337b10f4"
 };
 
 
