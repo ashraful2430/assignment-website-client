937a03c6ed35e77af174b15ca0e9df84fa26b1af
ashraful2430 <www.savage582@gmail.com>
2023-12-07 22:54:27 +0600

    change the firebase hosting

--- src/Config/firebase.config.js
+++ src/Config/firebase.config.js
@@ -1,16 +1,14 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDqwgeT4WV6NwAliIKDLyXXoK4dZpcm2Z8",
-    authDomain: "cars-doctor-958c0.firebaseapp.com",
-    projectId: "cars-doctor-958c0",
-    storageBucket: "cars-doctor-958c0.appspot.com",
-    messagingSenderId: "815479979549",
-    appId: "1:815479979549:web:912772ce582e81337b10f4"
+  apiKey: "AIzaSyAqsUgQ_OautjUJLJxoy2t2AFoxAA6UzrM",
+  authDomain: "learn-it-write-it.firebaseapp.com",
+  projectId: "learn-it-write-it",
+  storageBucket: "learn-it-write-it.appspot.com",
+  messagingSenderId: "849537468355",
+  appId: "1:849537468355:web:610c36fc5266733f035f76",
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
